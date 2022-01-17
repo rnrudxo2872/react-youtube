58928bac382773302c8c67769e290d602e9caf3a
unknown <rnrudxo2872@naver.com>
2022-01-18 00:08:18 +0900

    링크 추가

--- src/components/resultVideo.tsx
+++ src/components/resultVideo.tsx
@@ -5,8 +5,11 @@ import { getChannelInfo } from "../api";
 import { ChennalInfo } from "../interfaces/resultVideo.interface";
 
 export default function ResultVideo(props: Item) {
-  const { snippet } = props;
-  const { title, thumbnails, channelId, description } = snippet;
+  const {
+    snippet,
+    id: { videoId },
+  } = props;
+  const { title, thumbnails, channelId, channelTitle, description } = snippet;
   const { medium } = thumbnails;
   const parser = new DOMParser();
   const { isLoading, data } = useQuery<ChennalInfo>(
@@ -14,46 +17,55 @@ export default function ResultVideo(props: Item) {
     () => getChannelInfo(channelId)
   );
 
+  console.log(props);
+
   return (
-    <div className={styles.container}>
-      <section>
-        <img
-          src={medium.url}
-          alt={`${title}의 thumbnail.`}
-          width={medium.width}
-          height={medium.height}
-        />
-      </section>
-      <section className={styles["info-container"]}>
-        <section className={styles["top-info"]}>
-          <div className={styles["title-wrapper"]}>
-            <h1>
-              {parser.parseFromString(title, "text/html").body.textContent}
-            </h1>
-          </div>
-          <div className={styles["chennal-wrapper"]}>
-            <div className={styles["chennal-img"]}>
-              {isLoading ? null : (
-                <img
-                  src={data?.items[0].snippet.thumbnails.default.url}
-                  alt={`${data?.items[0].snippet.title} 채널 이미지`}
-                  width={
-                    (data?.items[0].snippet.thumbnails.default.width || 88) - 25
-                  }
-                  height={
-                    (data?.items[0].snippet.thumbnails.default.height || 88) -
-                    25
-                  }
-                />
-              )}
+    <a href={`/watch?v=${videoId}`}>
+      <div className={styles.container}>
+        <section>
+          <img
+            src={medium.url}
+            alt={`${title}의 thumbnail.`}
+            width={medium.width}
+            height={medium.height}
+          />
+        </section>
+        <section className={styles["info-container"]}>
+          <section className={styles["top-info"]}>
+            <div className={styles["title-wrapper"]}>
+              <h1>
+                {parser.parseFromString(title, "text/html").body.textContent}
+              </h1>
             </div>
-            <div></div>
+            <div className={styles["chennal-wrapper"]}>
+              <div className={styles["chennal-img"]}>
+                {isLoading ? null : (
+                  <img
+                    src={data?.items[0].snippet.thumbnails.default.url}
+                    alt={`${data?.items[0].snippet.title} 채널 이미지`}
+                    width={
+                      (data?.items[0].snippet.thumbnails.default.width || 88) -
+                      45
+                    }
+                    height={
+                      (data?.items[0].snippet.thumbnails.default.height || 88) -
+                      45
+                    }
+                  />
+                )}
+              </div>
+              <div className={styles["chennal-title"]}>
+                <a href={`https://www.youtube.com/channel/${channelId}`}>
+                  <span>{channelTitle}</span>
+                </a>
+              </div>
+            </div>
+          </section>
+          <div className={styles["description-wrapper"]}>
+            <span>{`${description}`}</span>
           </div>
         </section>
-        <div className={styles["description-wrapper"]}>
-          <span>{`${description.slice(0, 100)}...`}</span>
-        </div>
-      </section>
-    </div>
+      </div>
+    </a>
   );
 }
