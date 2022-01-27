8dd82f050475810635b323f6a4c87bbd95a9cb91
unknown <rnrudxo2872@naver.com>
2022-01-27 20:27:20 +0900

    home 수정

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { getPopularVideos } from "../api";
 import PopularVideo from "../components/popularVideo";
 import { IVideoDetail } from "../interfaces/watch.interface";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   const { data, isLoading } = useQuery<IVideoDetail>(["home", "popular"], () =>
@@ -9,7 +10,7 @@ export default function Home() {
   );
   console.log(data, "home");
   return (
-    <div>
+    <div className={styles.root}>
       {isLoading
         ? "now Loading..."
         : data?.items.map((video) => (
