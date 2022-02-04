04e061469abaa2016841f54e4d2be16483379e70
unknown <rnrudxo2872@naver.com>
2022-02-04 23:58:05 +0900

    home - popular video에서 a태그 에러 로그 출현 수정

--- src/components/popularVideo.tsx
+++ src/components/popularVideo.tsx
@@ -1,4 +1,6 @@
+import { MouseEvent, useRef } from "react";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { youtubeAtom } from "../atoms/youtube";
 import { ChennalInfo } from "../interfaces/resultVideo.interface";
@@ -22,51 +24,61 @@ export default function PopularVideo(props: DetailItem) {
     ["chennel", `${channelId}`],
     () => youtube.getChannelInfo(channelId)
   );
+  const channelRef = useRef<HTMLDivElement>(null);
+  const pageNavigate = useNavigate();
+  const onClickVideo = (event: MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLDivElement;
+    const except =
+      target.closest<HTMLDivElement>(`.${styles["chennal-title"]}`) ??
+      target.closest<HTMLDivElement>(`.${styles["chennal-img"]}`);
+    if (except) {
+      return;
+    }
+    pageNavigate(`/watch?v=${id}`);
+  };
 
   return (
-    <div className={styles.container}>
-      <a href={`/watch?v=${id}`}>
-        <section>
-          <img
-            src={medium.url}
-            alt={`${title}의 thumbnail.`}
-            width={"100%"}
-            height={"auto"}
-          />
+    <div className={styles.container} onClick={onClickVideo}>
+      <section>
+        <img
+          src={medium.url}
+          alt={`${title}의 thumbnail.`}
+          width={"100%"}
+          height={"auto"}
+        />
+      </section>
+      <section className={styles["info-container"]}>
+        <section className={styles["chennal-img"]}>
+          {isLoading ? null : (
+            <img
+              src={data?.items[0].snippet.thumbnails.default.url}
+              alt={`${data?.items[0].snippet.title} 채널 이미지`}
+              width={
+                (data?.items[0].snippet.thumbnails.default.width || 88) - 45
+              }
+              height={
+                (data?.items[0].snippet.thumbnails.default.height || 88) - 45
+              }
+            />
+          )}
         </section>
-        <section className={styles["info-container"]}>
-          <section className={styles["chennal-img"]}>
-            {isLoading ? null : (
-              <img
-                src={data?.items[0].snippet.thumbnails.default.url}
-                alt={`${data?.items[0].snippet.title} 채널 이미지`}
-                width={
-                  (data?.items[0].snippet.thumbnails.default.width || 88) - 45
-                }
-                height={
-                  (data?.items[0].snippet.thumbnails.default.height || 88) - 45
-                }
-              />
-            )}
-          </section>
-          <section className={styles["top-info"]}>
-            <div className={styles["title-wrapper"]}>
-              <h1>
-                {parser.parseFromString(title, "text/html").body.textContent}
-              </h1>
-            </div>
-            <div className={styles["chennal-title"]}>
-              <a href={`https://www.youtube.com/channel/${channelId}`}>
-                <span>{channelTitle}</span>
-              </a>
-            </div>
-            <div className={styles["video-info"]}>
-              <span>조회수 {getFormattedCounts(viewCount)}</span>
-              <span> • {getLater(publishedAt)}</span>
-            </div>
-          </section>
+        <section className={styles["top-info"]}>
+          <div className={styles["title-wrapper"]}>
+            <h1>
+              {parser.parseFromString(title, "text/html").body.textContent}
+            </h1>
+          </div>
+          <div className={styles["chennal-title"]} ref={channelRef}>
+            <a href={`https://www.youtube.com/channel/${channelId}`}>
+              <span>{channelTitle}</span>
+            </a>
+          </div>
+          <div className={styles["video-info"]}>
+            <span>조회수 {getFormattedCounts(viewCount)}</span>
+            <span> • {getLater(publishedAt)}</span>
+          </div>
         </section>
-      </a>
+      </section>
     </div>
   );
 }
